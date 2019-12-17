02158e414e79c47a594e11d1d157290fc15e0c6d
peiak <peikai@outlook.com>
2019-12-17 10:35:04 +0800

    find Li contained compounds

--- convex_hull.py
+++ convex_hull.py
@@ -1,42 +1,22 @@
-from pymatgen import MPRester, Composition
-from pymatgen.analysis.phase_diagram import (CompoundPhaseDiagram, PDEntry,
-                                             PDPlotter, PhaseDiagram, get_facets)
-import numpy as np
-
-
-def tieline_phases(pd, key_element):
-    # find its coordinate in phase diagram
-    comp = Composition(key_element)
-    c = pd.pd_coords(comp)
-    # find facets that key element acted as a vertice
-    facet_list = list()
-    for f, s in zip(pd.facets, pd.simplexes):
-        if s.in_simplex(c, PhaseDiagram.numerical_tol / 10):
-            facet_list.append(f)
-
-    # find other phases in those facets
-    vertice_array = np.array(facet_list).flatten()
-    tieline_phases = list(set(pd.qhull_entries[each].name for each in vertice_array))
-    tieline_phases.remove('Li')
-
-    return(tieline_phases)
+from pymatgen import MPRester
+import pandas as pd
 
 
 with MPRester(api_key='25wZTKoyHkvhXFfO') as mpr:
-    entries = mpr.get_entries_in_chemsys('Li-Al-N')
+    entries_binary = mpr.query(criteria={'elements':{'$all': ['Li']}, 'nelements':2}, properties=['material_id', 'pretty_formula'])
+    entries_ternary = mpr.query(criteria={'elements':{'$all': ['Li']}, 'nelements':3}, properties=['material_id', 'pretty_formula'])
+    entries_quaternary = mpr.query(criteria={'elements':{'$all': ['Li']}, 'nelements':4}, properties=['material_id', 'pretty_formula'])
+    entries_others = mpr.query(criteria={'elements':{'$all': ['Li']}, 'nelements':{'$gte':5}}, properties=['material_id', 'pretty_formula'])
 
-    pd = PhaseDiagram(entries)
-    formula_list = tieline_phases(pd, key_element='Li')
-    candidates = mpr.query(criteria={'pretty_formula':{'$in':formula_list}, 'band_gap': {'$lte': 2}}, properties=['material_id', 'pretty_formula'])
+binary_dataframe = pd.DataFrame(entries_binary)
+ternary_dataframe = pd.DataFrame(entries_ternary)
+quaternary_dataframe = pd.DataFrame(entries_quaternary)
+others_dataframe = pd.DataFrame(entries_others)
 
-# find the vertice that appear three times, which will be Li
-# main_vertice_id = np.argmax(np.bincount(vertice_array.flatten()))
+binary_dataframe.to_csv('Li_binary.csv', index=False)
+ternary_dataframe.to_csv('Li_ternary.csv', index=False)
+quaternary_dataframe.to_csv('Li_quaternary.csv', index=False)
+others_dataframe.to_csv('Li_others.csv', index=False)
 
-# plotter = PDPlotter(pd)
-# plotter.show()
 
-# data = np.array([[e.composition.get_atomic_fraction(el) for el in pd.elements] + [e.energy_per_atom] for e in pd.stable_entries])
-# print(len(data[:,1:]))
 
-# print(len(pd.qhull_entries))
-# print(len(pd.qhull_data))
