3e760e39c6f378b0f9cecd1dcd4a7621451158d1
Kai Pei <peikai@outlook.com>
2020-01-09 21:51:23 +0800

    meta-stage plot ternary pd with energy

--- 7.plot_ternary_pd_energy.py
+++ 7.plot_ternary_pd_energy.py
@@ -1,11 +1,34 @@
 import numpy as np
+import pandas
 import plotly
 import plotly.graph_objects as go
 from pymatgen import Composition, MPRester
 from pymatgen.analysis.phase_diagram import (PDPlotter, PhaseDiagram,
-                                             tet_coord, triangular_coord)
-from scipy.spatial import ConvexHull
+                                             tet_coord, triangular_coord, get_facets)
+from scipy.spatial import ConvexHull, Delaunay
 
+
+def merge_facet(convexhull):
+    # index each equiation with a feature
+    equitions = convexhull.equations
+    id_list = [(each[0]*1 + each[1]*2 + each[2]*3 + each[3]*4) for each in convexhull.equations]
+    dataframe = pandas.DataFrame(equitions)
+    dataframe['id'] = id_list
+    # find facets with the same hyperplane equation
+    unique_id = np.unique(id_list)
+    facet_list = []
+    for id in unique_id:
+        index_list = dataframe[dataframe.loc[:,'id'] == id].index
+        facet_array = convexhull.simplices[index_list]
+        facet_merged = np.unique(facet_array)
+        facet_points = convexhull.points[facet_merged]
+        # # remove points located within hyperplane
+        # hull_new = ConvexHull(facet_points)
+        # hull_vertices = hull_new.vertices
+        # facet_points = facet_points[hull_vertices]
+        facet_list.append(facet_points)
+        
+    return(facet_list)
 
 
 def plotly_polyhedron(polyhedron, color):
@@ -14,10 +37,10 @@ def plotly_polyhedron(polyhedron, color):
     convex_polyhedon = dict(x=x, y=y, z=z, type= 'mesh3d', alphahull=0, opacity=0.1, color=color)
     return(convex_polyhedon)
 
-def plotly_lines(facet):
+def plotly_lines(line_nodes):
     # To make lines a loop.
-    facet = np.vstack((facet, facet[0]))
-    x, y, z = [facet[:,0], facet[:,1], facet[:,2]]
+    line_nodes = np.vstack((line_nodes, line_nodes[0]))
+    x, y, z = [line_nodes[:,0], line_nodes[:,1], line_nodes[:,2]]
     lines=dict(x=x, y=y, z=z, mode='lines', type='scatter3d', showlegend=False, line=dict(color= 'rgb(50,50,50)', width=3))
     return(lines)
 
@@ -49,44 +72,42 @@ entries = pd.qhull_entries
 qhull_data = pd.qhull_data
 qhull_data = np.delete(qhull_data, -1, axis=0)
 
-qhull_cord = qhull_data[:, 0:2]
-# qhull_cord = np.vstack([triangular_coord(each) for each in qhull_cord])
-qhull_cord = np.vstack(qhull_cord)
-# qhull_x_values = 1.0 - qhull_data[:,0] - qhull_data[:,1]
-# qhull_data = np.insert(qhull_data, 0, values=qhull_x_values, axis=1)
 facet_vertices = pd.facets
-facet_cord = [qhull_cord[each] for each in facet_vertices]
-facet_energy = np.vstack([qhull_data[each] for each in facet_vertices])[:,-1]
+facet_cord = [qhull_data[each][:, 0:2] for each in facet_vertices] # multi-level array, contains duplicates vertices
+# add z=0, convert 2d to 3d
+facet_cord_3d = [np.insert(facet, 2, values=0, axis=1) for facet in facet_cord]
+# qhull_cord = np.vstack([triangular_coord(each) for each in qhull_cord])
 
-# duplicated nodes are regardless
-stable_nodes = np.vstack(facet_cord)
+# do not need duplicated nodes using for construct convex hull
+stable_nodes = np.unique(facet_vertices)
+stable_cord = qhull_data[stable_nodes][:,0:2]
+stable_energy = qhull_data[stable_nodes][:,2]
+
 # nodes of projections of convex hull construction
-nodes_bottom = np.insert(stable_nodes, 2, values=0, axis=1)
-# nodes regardless projections
-nodes_top_z = facet_energy[:,np.newaxis]
-nodes_top = np.insert(stable_nodes, [2], values=nodes_top_z, axis=1)
+nodes_bottom = np.insert(stable_cord, 2, values=0, axis=1)
+# reshape array (3,) to (3,1)
+nodes_top_z = stable_energy[:,np.newaxis]
+nodes_top = np.insert(stable_cord, [2], values=nodes_top_z, axis=1)
 
 # merge two parts of nodes
 nodes_array = np.append(nodes_bottom, nodes_top, axis=0)
-# qhull_x_values = 1.0 - qhull_data[:,0] - qhull_data[:,1]
-# qhull_data = np.insert(qhull_data, 0, values=qhull_x_values, axis=1)
-
 data = []
 
-for facet in facet_cord:
-    # add z=0, convert 2d to 3d
-    facet = np.insert(facet, 2, values=0, axis=1)
+for facet in facet_cord_3d:
     convex_lines = plotly_lines(facet)
     data.append(convex_lines)
 
 polyhedron = plotly_polyhedron(nodes_array, color="rgb(106, 90, 205)")
 data.append(polyhedron)
 
-hull = ConvexHull(nodes_array)
-for simplex in hull.simplices:
-    facet = nodes_array[simplex]
-    convex_lines = plotly_lines(facet)
-    data.append(convex_lines)
+# hull = ConvexHull(nodes_array)
+# # merge triangle facets which belongs in the same plane to rectangle facets.
+# facet_list = merge_facet(hull)
+
+# for facet_vertices in facet_list:
+#     # line_nodes = nodes_array[face_simplices]
+#     edge = plotly_lines(facet_vertices)
+#     data.append(edge)
 
 # nodes_index = np.array(facet_vertices).flatten()
 # nodes_index = np.unique(nodes_index)
