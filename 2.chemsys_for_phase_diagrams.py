f4da23e0a7926a3caf5fa12f436be375f5b5f084
Kai Pei <peikai@outlook.com>
2020-02-22 14:35:18 +0800

    adjust dataframe to series in drop_subset function

--- 2.chemsys_for_phase_diagrams.py
+++ 2.chemsys_for_phase_diagrams.py
@@ -11,7 +11,8 @@ def drop_subset_chemsys(chemsys_series):
     # prepare a list for iterations
     chemsys_list = chemsys_series.to_list()
     # prepare a dataframe to store other info
-    chemsys_dataframe = pd.DataFrame(chemsys_series, columns=['pretty_formula', 'distinct'])
+    chemsys_dataframe = pd.DataFrame(chemsys_series, columns=['pretty_formula'])
+    chemsys_dataframe['distinct'] = 'null'
     # like, A-B is not a subset of any chemsys, then store A-B
     for index, row in chemsys_series.iteritems():
         boolean =  [row.issubset(chemsys) for chemsys in chemsys_list]
@@ -19,9 +20,10 @@ def drop_subset_chemsys(chemsys_series):
         if not boolean.count(True) > 1:
             chemsys_dataframe.loc[index, 'distinct'] = True
 
-    chemsys_distinct = chemsys_dataframe.loc[chemsys_dataframe['distinct'] == True] 
-    chemsys_distinct.loc[:, 'chemsys'] = chemsys_distinct.pretty_formula.apply(lambda x : '-'.join(x))
-    return chemsys_distinct
+    chemsys_distinct_dataframe = chemsys_dataframe.loc[chemsys_dataframe['distinct'] == True] 
+    chemsys_distinct_dataframe.loc[:, 'chemsys'] = chemsys_distinct_dataframe.pretty_formula.apply(lambda x : '-'.join(x))
+    chemsys_distinct_series = chemsys_distinct_dataframe['chemsys']
+    return chemsys_distinct_series
        
 
 chemsys_all = pd.read_csv('tables/K/K_all.csv', usecols=['pretty_formula'])
@@ -31,4 +33,4 @@ chemsys_all= chemsys_all.pretty_formula.apply(lambda x : '-'.join([e.name for e 
 chemsys_all.drop_duplicates(inplace=True)
 
 chemsys_all = drop_subset_chemsys(chemsys_all)
-chemsys_all.to_csv('chemsys_all.csv', columns=['chemsys'], index=False)
+chemsys_all.to_csv('chemsys_all.csv', header=['chemsys'], index=False)
