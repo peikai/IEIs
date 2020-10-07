bfebe93b0637a668828ea2c1849f9c9ead588915
peikai <pk.edu.cn@outlook.com>
2020-10-07 16:07:04 +0800

    convert csv table to html table

--- functional_tools/dataframe_to_html_table.py
+++ functional_tools/dataframe_to_html_table.py
@@ -1,9 +1,12 @@
 import pandas as pd 
 
-table_calc = pd.read_csv('tables/K/K_candidates_calc.csv')
-table_exp = pd.read_csv('tables/K/K_candidates_exp.csv')
-table_all = pd.read_csv('tables/K/K_candidates_all.csv')
+# table_calc = pd.read_csv('tables/K/K_candidates_calc.csv')
+# table_exp = pd.read_csv('tables/K/K_candidates_exp.csv')
+# table_all = pd.read_csv('tables/K/K_candidates_all.csv')
 
-table_calc.to_html('tables/K/K_candidates_calc.html')
-table_exp.to_html('tables/K/K_candidates_exp.html')
-table_all.to_html('tables/K/K_candidates_all.html')
+# table_calc.to_html('tables/K/K_candidates_calc.html')
+# table_exp.to_html('tables/K/K_candidates_exp.html')
+# table_all.to_html('tables/K/K_candidates_all.html')
+
+table = pd.read_csv('tables/Merged/fullwindow_all_Li.csv')
+table.to_html('fullwindow_all_Li.html')
