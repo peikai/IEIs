ca367052ef2345cb8e3a49de99a52441a9884b7d
peikai <pk.edu.cn@outlook.com>
2020-12-03 01:15:44 +0800

    add comments to point out GGA/GGA+U mixed scheme

--- 2.extract_facets_and_tielined_phases.py
+++ 2.extract_facets_and_tielined_phases.py
@@ -36,7 +36,7 @@ def get_phase_diagram_in_chemsys(chemsys):
     eventlet.monkey_patch() 
     with eventlet.Timeout(seconds=120, exception=True) as timeout:
         with MPRester(api_key='25wZTKoyHkvhXFfO') as mpr:
-            # using GGA and GGA+U mixed mode as default, namely compatible_only=True
+            # using GGA and GGA+U mixed scheme as default, namely compatible_only=True
             entries = mpr.get_entries_in_chemsys(chemsys)
             
     phase_diagram = PhaseDiagram(entries)
