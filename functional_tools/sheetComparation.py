94b475310130b16dc1a39f59017de25b5ab709be
peikai <pk.edu.cn@outlook.com>
2020-12-04 19:36:19 +0800

    Multi-ion and Electron Insulators

--- functional_tools/sheetComparation.py
+++ functional_tools/sheetComparation.py
@@ -2,27 +2,30 @@ import pandas as pd
 
 
 # [option-1] Archive result A and present result B
-# data1 = pd.read_csv('Archive-2020.11.21/tables/Merged/candidates_all_K.csv', usecols=['material_id', 'pretty_formula'])
-# data2 = pd.read_csv('Tables/K/candidates.csv', usecols=['material_id', 'pretty_formula'])
+# set1 = pd.read_csv('Archive-2020.11.21/tables/Merged/candidates_all_K.csv', usecols=['material_id', 'pretty_formula'])
+# set2 = pd.read_csv('Tables/K/candidates.csv', usecols=['material_id', 'pretty_formula'])
 
 # [option-2] Set A and subset B
-data1 = pd.read_csv('Tables/Li/fullwindow.csv', usecols=['material_id', 'pretty_formula'], index_col='material_id')
-data2 = pd.read_csv('Tables/Li/candidates.csv', usecols=['material_id', 'pretty_formula'], index_col='material_id')
+set1 = pd.read_csv('Tables/Na/candidates.csv', usecols=['material_id', 'pretty_formula'], index_col='material_id')
+set2 = pd.read_csv('Tables/Li/candidates.csv', usecols=['material_id', 'pretty_formula'], index_col='material_id')
 
 # A U B - A ∩ B
-data_merged = pd.merge(data1, data2, how='outer', on=['material_id', 'pretty_formula'])
-data_merged.drop_duplicates(keep=False, inplace=True)
-# data_merged.reset_index()
+set_difference = pd.merge(set1, set2, how='outer', on=['material_id', 'pretty_formula'])
+set_difference.drop_duplicates(keep=False, inplace=True)
+# set_merged.reset_index()
+
+# A ∩ B
+set_intersection = pd.merge(set1, set2, how='inner', on=['material_id', 'pretty_formula'])
 
 # A - A ∩ B
-data1_intersect_data2 = pd.merge(data1, data2, how='inner', on=['material_id', 'pretty_formula'])
-data1_contained = data1.append(data1_intersect_data2)
-data1_contained = data1_contained.drop_duplicates(keep=False)
+set1_contains_only = set1.append(set_intersection)
+set1_contains_only = set1_contains_only.drop_duplicates(keep=False)
 
 # B - A ∩ B
-data2_contained = data2.append(data1_intersect_data2)
-data2_contained = data2_contained.drop_duplicates(keep=False)
+set2_contains_only = set2.append(set_intersection)
+set2_contains_only = set2_contains_only.drop_duplicates(keep=False)
 
-data_merged.sort_index(axis=0).to_csv('set_universal.0.csv', index=False)
-data1_contained.sort_index(axis=0).to_csv('set_difference.1.csv', index=False)
-data2_contained.sort_index(axis=0).to_csv('set_difference.2.csv', index=False)
+set_difference.sort_index(axis=0).to_csv('set_difference.csv')
+set1_contains_only.sort_index(axis=0).to_csv('set_difference.1.csv')
+set2_contains_only.sort_index(axis=0).to_csv('set_difference.2.csv')
+set_intersection.sort_index(axis=0).to_csv('set_intersection.csv')
