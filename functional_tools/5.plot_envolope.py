1d54151d1a6224782f109163644f011d93418cc6
peikai <pk.edu.cn@outlook.com>
2021-01-18 00:00:00 +0800

    format the plot function

--- functional_tools/5.plot_envolope.py
+++ functional_tools/5.plot_envolope.py
@@ -8,16 +8,20 @@ import plotly
 from pymatgen import Composition, MPRester
 from pymatgen.analysis.phase_diagram import PhaseDiagram
 
+
 def plotly_lines(line_nodes, dash, width):
-    x, y, z = [line_nodes[:,0], line_nodes[:,1], line_nodes[:,2]]
-    lines=dict(x=x, y=y, z=z, mode='lines', type='scatter3d', showlegend=False, line=dict(dash=dash, color='rgb(50,50,50)', width=width))
+    x, y, z = [line_nodes[:, 0], line_nodes[:, 1], line_nodes[:, 2]]
+    lines = dict(x=x, y=y, z=z, mode='lines', type='scatter3d', showlegend=False,
+                 line=dict(dash=dash, color='rgb(50,50,50)', width=width))
     return lines
+
 
 def plotly_hulls(x, y, z, i, j, k, color):
     hull = dict(x=x, y=y, z=z, i=i, j=j, k=k, type='mesh3d', opacity=1, color=color,
-                lighting=dict(ambient=0.8, diffuse=1, specular=1), 
+                lighting=dict(ambient=0.8, diffuse=1, specular=1),
                 lightposition=dict(x=100000, y=100000, z=-100000))
     return hull
+
 
 def REST_local(chemsys):
     if not os.path.exists('temp/entries_{v}.pickle'.format(v=chemsys)):
@@ -30,51 +34,50 @@ def REST_local(chemsys):
             entries = pickle.load(entries_temp)
     return entries
 
+
 def plot_envolope(pd):
     # get original qhull data
     qhull_data = pd.qhull_data
-
-    # simplices without irrelevant facets
-    facet_vertices = pd.facets # multi-level array, contains duplicates vertices
-
     # an extra point exists in original pd.qhull_data, drop it
     qhull_data = np.delete(qhull_data, -1, axis=0)
 
+    # simplices without irrelevant facets
+    facet_vertices = pd.facets  # multi-level array, contains duplicates vertices
     # convert vertex cordinations of each facet
     facet_cord_list = [qhull_data[each] for each in facet_vertices]
-
     # do not need duplicated nodes using for construct convex hull
     stable_nodes_index = np.unique(facet_vertices)
     # split cords and energies of stable entries from qhull data
-    stable_cord_xy = qhull_data[stable_nodes_index][:,0:2]
-    stable_energy = qhull_data[stable_nodes_index][:,2]
-
+    stable_cord_xy = qhull_data[stable_nodes_index][:, 0:2]
+    stable_energy = qhull_data[stable_nodes_index][:, 2]
     # nodes of projected hull
     nodes_projected = np.insert(stable_cord_xy, 2, values=0, axis=1)
     # reshape array (3,) to (3,1)
-    nodes_stable_z = stable_energy[:,np.newaxis]
-    nodes_stable = np.insert(stable_cord_xy, [2], values=nodes_stable_z, axis=1)
+    nodes_stable_z = stable_energy[:, np.newaxis]
+    nodes_stable = np.insert(
+        stable_cord_xy, [2], values=nodes_stable_z, axis=1)
 
     # plot section
     data = []
 
     # plot scatters of stable nodes
     scatter_vertices = dict(
-        mode = "markers",
-        name = 'nodes_stable',
-        type = "scatter3d",
-        x = nodes_stable[:,0], y = nodes_stable[:,1], z = nodes_stable[:,2],
-        marker = dict(size=6, color="rgb(67, 147, 195)")
+        mode="markers",
+        name='nodes_stable',
+        type="scatter3d",
+        x=nodes_stable[:, 0], y=nodes_stable[:, 1], z=nodes_stable[:, 2],
+        marker=dict(size=6, color="rgb(67, 147, 195)")
     )
     data.append(scatter_vertices)
 
     # plot scatters of projected nodes
     scatter_vertices = dict(
-        mode = "markers",
-        name = 'nodes_projected',
-        type = "scatter3d",
-        x = nodes_projected[:,0], y = nodes_projected[:,1], z = nodes_projected[:,2],
-        marker = dict(size=6, color="rgb(50,50,50)")
+        mode="markers",
+        name='nodes_projected',
+        type="scatter3d",
+        x=nodes_projected[:, 0], y=nodes_projected[:,
+                                                   1], z=nodes_projected[:, 2],
+        marker=dict(size=6, color="rgb(50,50,50)")
     )
     data.append(scatter_vertices)
 
@@ -91,7 +94,7 @@ def plot_envolope(pd):
         data.append(convex_lines)
         # change z to zero, project facets to bottom
         edge_copy = edge.copy()
-        edge_copy[:,2] = 0
+        edge_copy[:, 2] = 0
         # plot facets projections
         convex_lines = plotly_lines(edge_copy, dash='solid', width=4)
         data.append(convex_lines)
@@ -99,37 +102,41 @@ def plot_envolope(pd):
     # plot potential-energy envolope, see https://plotly.com/python/3d-mesh/
     x, y, z = qhull_data.T
     i, j, k = np.array(facet_vertices).T
-    hull = plotly_hulls(x,y,z,i,j,k,color='rgb(146,197,222)')
+    hull = plotly_hulls(x, y, z, i, j, k, color='rgb(146,197,222)')
     data.append(hull)
 
     layout = dict(
-        title_x = 0.5,
-        scene = dict(xaxis = dict(zeroline=False, showticklabels=False, showgrid=False, showline=False, title = {'text':''}, visible=False),
-                    yaxis = dict(zeroline=False, showticklabels=False, showgrid=False, showline=False, title = {'text':''}, visible=False),
-                    zaxis = dict(zeroline=False, showticklabels=False, showgrid=False, showline=False, title = {'text':''}, visible=False),
-                    camera= dict(eye = dict(x=2.0, y=1.0, z=0.5)),
-                    ),
+        title_x=0.5,
+        scene=dict(xaxis=dict(zeroline=False, showticklabels=False, showgrid=False, showline=False, title={'text': ''}, visible=False),
+                   yaxis=dict(zeroline=False, showticklabels=False, showgrid=False,
+                              showline=False, title={'text': ''}, visible=False),
+                   zaxis=dict(zeroline=False, showticklabels=False, showgrid=False,
+                              showline=False, title={'text': ''}, visible=False),
+                   camera=dict(eye=dict(x=2.0, y=1.0, z=0.5)),
+                   ),
         showlegend=False
     )
 
     fig = dict(data=data, layout=layout)
     return fig
 
+
 def main():
     # [option] plot in batch
-        # pretty_formula_list = pandas.read_csv('tables/Li/candidates.csv').pretty_formula.to_list()
-        # chemsys_list = [Composition(each).chemical_system+'-Li' for each in pretty_formula_list if len(Composition(each).elements)==2]
-        # for i, chemsys in enumerate(chemsys_list):
-        #     plot_envolope(chemsys)
-        #     print('{I}/{L}'.format(I=i+1, L=len(chemsys_list)))
-    
+    # pretty_formula_list = pandas.read_csv('tables/Li/candidates.csv').pretty_formula.to_list()
+    # chemsys_list = [Composition(each).chemical_system+'-Li' for each in pretty_formula_list if len(Composition(each).elements)==2]
+    # for i, chemsys in enumerate(chemsys_list):
+    #     plot_envolope(chemsys)
+    #     print('{I}/{L}'.format(I=i+1, L=len(chemsys_list)))
+
     # [option] plot a given chemical system
     chemsys = 'O-Lu-Li'
     entries = REST_local(chemsys)
     pd = PhaseDiagram(entries)
     fig = plot_envolope(pd)
-    plotly.offline.plot(fig, filename='{fn}.html'.format(fn=chemsys), show_link=False, auto_open=False)
+    plotly.offline.plot(fig, filename='{fn}.html'.format(
+        fn=chemsys), show_link=False, auto_open=False)
+
 
 if __name__ == "__main__":
     main()
-
