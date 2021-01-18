9719dd9be4f1cc9f804c69cffc5e35631295edfb
peikai <pk.edu.cn@outlook.com>
2021-01-19 02:36:03 +0800

    add comments to explain the code function

--- functional_tools/find_critical_potentials.py
+++ functional_tools/find_critical_potentials.py
@@ -1,11 +1,11 @@
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from pymatgen import Composition, Element, MPRester
-from pymatgen.analysis.phase_diagram import PhaseDiagram, GrandPotentialPhaseDiagram
+from pymatgen.analysis.phase_diagram import PhaseDiagram
 
-key_element = 'Na'
+
+key_element = 'Li'
 target_phase = 'BeO'
 
 chemsys = key_element + '-' + Composition(target_phase).chemical_system
@@ -15,19 +15,20 @@ with MPRester(api_key='25wZTKoyHkvhXFfO') as mpr:
 pd = PhaseDiagram(entries)
 
 for facet in pd.facets:
-    chempots = pd._get_facet_chempots(facet)
+    # read-made, chempots = pd._get_facet_chempots(facet)
+    # implement again
     namelist = [pd.qhull_entries[i].name for i in facet]
     complist = [pd.qhull_entries[i].composition for i in facet]
     energylist = [pd.qhull_entries[i].energy_per_atom for i in facet]
     m = [[c.get_atomic_fraction(e) for e in pd.elements] for c in
             complist]
-
+    # solve a linear matrix equation, sum(μ*n) = E
     chempots = np.linalg.solve(m, energylist)
     chempots = dict(zip(pd.elements, chempots))
     mu = chempots[Element(key_element)]
-    print('-'.join(namelist))
-    print(np.round(mu,3))
+    # print chemical potential of key element in each equilibrium
+    print('-'.join(namelist)+': '+str(np.round(mu,3)))
 
-# print(pd.get_chempot_range_map([Element(key_element)]))
+# print transition points of phase equilibria in terms of chemical potential
 chempotlist = np.round(pd.get_transition_chempots(Element(key_element)), 3)
 print(chempotlist)
